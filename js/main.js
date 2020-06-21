261a4d3341af75c595dabf9ee536c8f5a590cb58
Kevin <j309028133@gmail.com>
2020-06-21 15:46:25 +0800

    feat():新增輪播特效>同樣grid倒轉

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-$('#banner').slick();
+$('#banner').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false
+});
+$('#brand').slick({
+    autoplay:true,
+    autoplaySpeed: 1000,
+    dots: true,
+    slidesToShow: 4,
+    arrows: false
+});
